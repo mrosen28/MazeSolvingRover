4cddd002d957f0dd5878192088819d7e33d4418c
Mike Rosenberg <mrosen28@binghamton.edu>
2017-05-01 18:38:38 -0400

    Follow Left Line

--- main.c
+++ main.c
@@ -2,12 +2,6 @@
 #include "motor.c"
 #include "qtr_driver.c"
 
-
-void driveforward();
-void turnLeft();
-void turnRight();
-void uturn();
-
 int main() {
   //RoverSetup
   init_motors ();
@@ -19,94 +13,53 @@ int main() {
 
     //Get Sensor Values
     _delay_ms(1);
-    unsigned char checkSensorValue = get_QTR_value() & 0x0F; //Only using B(3:0)
+    ////unsigned char leftTurnAvail = 0;
+    //unsigned char rightTurnAvail = 0;
+    //unsigned char straightAvail = 0;
+    //char turnDirection;
+    unsigned char checkSensorValue = get_QTR_value() & 0x07;// & 0x0F; //Only using B(3:0)
     PORTC = checkSensorValue; //allows to check if sensors are outputting correctly on C
-    //	d0 = rightmost sensor
-	  //  d1 = right center sensor
-	  //  d2 = left center sensor
-	  //  d3 = left sensor
+    //	d0 = leftmost sensor
+	  //  d1 = middle
+	  //  d2 = rightmost Sensor
 
-    if((checkSensorValue & 0x08) == 0){
-      //Left Turn Available
-      turnLeft();
-      continue;
+    //
+    if(checkSensorValue == 0x01){
+      leftmotor_forward();
+      rightmotor_forward();
+      _delay_ms(2);
+      leftmotor_brake();
+      rightmotor_brake();
+      _delay_ms(1);
     }
-    else if(checkSensorValue & 0x09 ){
-      //No Left Turn
-      driveforward();
-      continue;
+    else if(checkSensorValue == 0x00){
+      leftmotor_coast();
+      rightmotor_forward();
+      _delay_ms(2);
+      leftmotor_brake();
+      rightmotor_brake();
+      _delay_ms(1);
     }
-    else if((checkSensorValue & 0x01) == 0){
-      turnRight();
-      continue;
+    else if(checkSensorValue == 0x04){
+      leftmotor_forward();
+      rightmotor_coast();
+      _delay_ms(2);
+      leftmotor_brake();
+      rightmotor_brake();
+      _delay_ms(1);
     }
-    else if(checkSensorValue & 0x0F){
-      //Can Not Advance: uTurn
-      uturn();
-      continue;
+    else if(checkSensorValue == 0x07){
+      while(checkSensorValue != 0x01){
+        leftmotor_reverse();
+        rightmotor_forward();
+        _delay_ms(2);
+        leftmotor_brake();
+        rightmotor_brake();
+        _delay_ms(1);
+        checkSensorValue = get_QTR_value() & 0x03;
+      }
     }
 }
 
-  return 1;
-}
-
-void driveforward(){
-  unsigned char testSensorValue = get_QTR_value() & 0x06; //Check Center Sensors
-
-  //StayOnLine
-  if(!testSensorValue){
-    //OnLine: Proceed
-    leftmotor_forward();
-    rightmotor_forward();
-  }
-  else if(testSensorValue == 0x02){
-    //OffLine(RightSide): Compensating
-    leftmotor_coast();
-    rightmotor_forward();
-  }
-  else if(testSensorValue == 0x04){
-    //OffLine(LeftSide): Compensating
-    leftmotor_forward();
-    rightmotor_coast();
-  }
-
-}
-
-void uturn(){
-  unsigned char checkSensorValue = get_QTR_value() & 0x0F;
-
-  //While All Sensors Show Black, Turn Left
-  while (checkSensorValue & 0x06){
-    leftmotor_reverse();
-    rightmotor_forward();
-    checkSensorValue = get_QTR_value() & 0x0F;
-  }
-}
-
-void turnLeft(){
-  unsigned char checkSensorValue = get_QTR_value() & 0x0F;
-  while((checkSensorValue & 0x08) == 0){
-    leftmotor_reverse();
-    rightmotor_forward();
-    checkSensorValue = get_QTR_value() & 0x0F;
-  }
-  while (checkSensorValue & 0x06) {
-    leftmotor_reverse();
-    rightmotor_forward();
-    checkSensorValue = get_QTR_value() & 0x0F;
-  }
-}
-
-void turnRight(){
-  unsigned char checkSensorValue = get_QTR_value() & 0x0F;
-  while((checkSensorValue & 0x01) == 0){
-    leftmotor_reverse();
-    rightmotor_forward();
-    checkSensorValue = get_QTR_value() & 0x0F;
-  }
-  while (checkSensorValue & 0x06) {
-    leftmotor_reverse();
-    rightmotor_forward();
-    checkSensorValue = get_QTR_value() & 0x0F;
-  }
+  return 0;
 }
